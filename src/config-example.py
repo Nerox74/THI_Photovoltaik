42c1e5a457593a2fa5efc439d14cc4d817ae670c
Nils Schaeftlein <schaeftlein.nils@gmail.com>
2026-06-15 10:54:26 +0200

    API

--- src/config-example.py
+++ src/config-example.py
@@ -1,5 +1,2 @@
-<<<<<<< HEAD
+
 """ Beispiel Example Datei, wird mit ins Gihtub repository hochgeladen"""
-=======
-""" Hier wird die Config für Github beispielhaft dargestellt, sodass man weiß wie diese Aufgebaut ist"""
->>>>>>> 83ce448ecdc187d5008bbb09172ad408ce1fe594
--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -1,9 +1,38 @@
 """Stellt eine Verbindung zu Prometheus her und holt von dort die CSV-Dateien, bereinigt die Daten und speichert die bereinigten Daten in cleaned_data.csv"""
+from streamlit.runtime.credentials import Credentials
 
 # Imports
+import requests
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+import urllib3
+
+#Wenn nicht funktioniert OPC Server -> exponentielle Wartezeit
+
+#Ordner in welchem env File liegt --> 1 Ornder über src
+env_path = Path(__file__).resolve().parent.parent / ".env"
+
+#IN Environ Laden
+load_dotenv(dotenv_path=env_path)
+
+#Jetzt kann über environ auf das File zugegriffen werden
+URL = os.environ.get("JUPYTER_HUB_URL")
+API_KEY= os.environ.get("API_KEY")
 
 
-# Überlegen wie live Daten geholt werden können --> ist immer live
+# Unterdrückt die "InsecureRequestWarning" (weil wir mit verify=False arbeiten)
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+# Genau derselbe Header wie im funktionierenden curl: "X-API-Key"
+headers = {"X-API-Key": API_KEY.strip(), "Accept": "application/json"}
+
+response = requests.get(URL, headers=headers, verify=False)
+
+print("Status:", response.status_code)
+print(response.json())
+
+
 
 
 
@@ -19,12 +48,14 @@ class PrometheusDatenbeschaffung:
         Stellt eine Verbindung zum Prometheus Server her, sodass die Daten von diesem abgerufen werden können.
 
         """
+        pass
 
 
     def data_loader(self) -> dict:
         """
         Nachdem die Verbindung mit Prometheus hergestellt worden ist, können jetzt die Daten geladen werden
         """
+        pass
 
 
     def data_cleaner(self,daten: dict) -> dict:
@@ -33,3 +64,4 @@ class PrometheusDatenbeschaffung:
         Format welches hier ausgegeben werden soll: (KWh, Zeit)
 
          """
+        pass
